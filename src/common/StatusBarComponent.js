6b8f3bf6cbd33f790f4471dc88f0ffbb90af75c1
Chinedu Ofor <oforchinedukelechi@gmail.com>
2018-03-05 23:19:32 +0100

    add landing page - add sign up page - add sign in page

---
+++ src/common/StatusBarComponent.js
@@ -0,0 +1,16 @@
+// react libraries
+import React from 'react';
+
+// react-native libraries
+import { StatusBar } from 'react-native'
+
+const StatusBarComponent = () => {
+  return (
+    <StatusBar
+      backgroundColor="white"
+      hidden = {false}
+    />
+  )
+}
+
+export { StatusBarComponent };
