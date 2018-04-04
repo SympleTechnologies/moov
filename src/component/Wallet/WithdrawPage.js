b0861710507fc10b98f6c92f56bf7513a83c7723
andela-cofor <chinedu.ofor@andela.com>
2018-04-04 04:43:20 +0100

    add transfer page

--- src/component/Wallet/WithdrawPage.js
+++ src/component/Wallet/WithdrawPage.js
@@ -13,11 +13,16 @@ import { Dropdown } from 'react-native-material-dropdown';
 
 // common
 import {ButtonTextComponent, StatusBarComponent} from "../../common/index";
+import Toast from "react-native-simple-toast";
 
 class WithdrawPage extends React.Component {
   state= {
     userToken: '',
-    requestSlot: 'LOAD'
+    requestSlot: 'LOAD',
+
+    accNumber: '',
+    bankNumber: '',
+    amount: ''
   };
 
   /**
@@ -30,33 +35,102 @@ class WithdrawPage extends React.Component {
     AsyncStorage.getItem("token").then((value) => {
       this.setState({ userToken: value });
     }).done();
+    AsyncStorage.getItem("user").then((value) => {
+      this.setState({ user: JSON.parse(value) });
+    }).done();
+  };
 
-    // this.chargeCard();
-  }
 
-  chargeCard() {
+  /**
+   * verifyAmount
+   *
+   * verifies amount input
+   * @return {void}
+   */
+  verifyBankNumber = () => {
+    let pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
 
-    RNPaystack.chargeCard({
-      cardNumber: '507850785078507812',
-      expiryMonth: '10',
-      expiryYear: '22',
-      cvc: '081',
-      email: 'oforchinedukelechi@gmail.com',
-      amountInKobo: 150000,
-    })
-      .then(response => {
-        console.log(response); // card charged successfully, get reference here
-      })
-      .catch(error => {
-        console.log(error); // error is a javascript Error object
-        console.log(error.message);
-        console.log(error.code);
-      })
+    if(this.state.bankNumber < 1) {
+      Toast.showWithGravity(`Invalid bank number`,Toast.LONG,Toast.TOP,);
+    } else if(this.state.bankNumber.length >= 3) {
+      if (!this.state.bankNumber.match(/[a-z]/i) && /^[a-zA-Z0-9- ]*$/.test(this.state.bankNumber) === true && this.state.bankNumber.length >= 3) {
+        return true
+      }
+      else {
+        Toast.showWithGravity(`Bank number should contain only numbers`,Toast.LONG,Toast.TOP,);
+      }
+    }
+    else {
+      Toast.showWithGravity(`Bank number should be 3 digits or more`,Toast.LONG, Toast.TOP,);
+    }
+  };
 
-  }
+  /**
+   * verifyAmount
+   *
+   * verifies amount input
+   * @return {void}
+   */
+  verifyAccNumber = () => {
+    let pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+
+    if(this.state.accNumber < 1) {
+      Toast.showWithGravity(`Invalid account number`,Toast.LONG,Toast.TOP,);
+    } else if(this.state.accNumber.length === 10) {
+      if (!this.state.accNumber.match(/[a-z]/i) && /^[a-zA-Z0-9- ]*$/.test(this.state.accNumber) === true && this.state.accNumber.length >= 3) {
+        return true
+      }
+      else {
+        Toast.showWithGravity(`Bank account number should contain only numbers`,Toast.LONG,Toast.TOP,);
+      }
+    }
+    else {
+      Toast.showWithGravity(`Bank account number should be 10 digits or more`,Toast.LONG, Toast.TOP,);
+    }
+  };
+
+  /**
+   * verifyAmount
+   *
+   * verifies amount input
+   * @return {void}
+   */
+  verifyAmount = () => {
+    let pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+
+    if(this.state.amount < 1) {
+      Toast.showWithGravity(`Invalid amount`,Toast.LONG,Toast.TOP,);
+    } else if(this.state.amount.length >= 3) {
+      if (!this.state.amount.match(/[a-z]/i) && /^[a-zA-Z0-9- ]*$/.test(this.state.amount) === true && this.state.amount.length >= 3) {
+        return true
+      }
+      else {
+        Toast.showWithGravity(`Amount should contain only numbers`,Toast.LONG,Toast.TOP,);
+      }
+    }
+    else {
+      Toast.showWithGravity(`Amount should be 3 digits or more`,Toast.LONG, Toast.TOP,);
+    }
+  };
+
+
+  /**
+   * withdraw
+   *
+   * Transfers user's money from wallet to bank account
+   * @return {void}
+   */
+  withdraw = () => {
+    if(this.verifyBankNumber()) {
+     if(this.verifyAccNumber()) {
+       if(this.verifyAmount()) {
+         Toast.showWithGravity(`Call server`,Toast.LONG, Toast.TOP,);
+       }
+     }
+    }
+  };
 
   render() {
-    console.log(this.state);
     let { height, width } = Dimensions.get('window');
     let slots = [ { value: 'LOAD', }, { value: 'TRANSFER', }, { value: 'WITHDRAW', } ];
 
@@ -65,39 +139,52 @@ class WithdrawPage extends React.Component {
         <StatusBarComponent backgroundColor='#fff' barStyle="dark-content" />
         <View style={{ flexDirection: 'column'}}>
           <View style={{ flexDirection: 'row', width: '70%', marginBottom: 10 }}>
-            <View style={{ paddingTop: width / 34 }}>
-              <Title>Enter Amount: </Title>
+            <View style={{ paddingTop: width / 28 }}>
+              <Title>Enter Bank Number: </Title>
             </View>
             <View style={{ width: '70%'}}>
-              <FormInput
-                style={{ flexDirection: 'row', width: 1 }}
-                keyboardType='numeric'
-                placeholder={''}
+              <TextInput
+                placeholder={'0000000'}
                 // onChangeText={...}
+                autoFocus
+                keyboardType='numeric'
+                onChangeText={amount => this.setState({ bankNumber: amount.replace(" ", "") })}
               />
             </View>
           </View>
-          <View style={{ flexDirection: 'row', width: '70%'}}>
-            <View style={{ paddingTop: width / 12 }}>
-              <Title>Transaction: </Title>
+          <View style={{ flexDirection: 'row', width: '70%', marginBottom: 10 }}>
+            <View style={{ paddingTop: width / 28 }}>
+              <Title>Enter Acc Number: </Title>
             </View>
-            <View style={{ width: '80%'}}>
-              <View style={{ width: '70%', zIndex: -1, marginLeft: width / 10}}>
-                <Dropdown
-                  // label='slots'
-                  // itemColor='blue'
-                  data={slots}
-                  value='LOAD'
-                  onChangeText={ requestSlot => this.setState({ requestSlot }) }
-                />
-              </View>
+            <View style={{ width: '70%'}}>
+              <TextInput
+                placeholder={'0000000'}
+                // onChangeText={...}
+                autoFocus
+                keyboardType='numeric'
+                onChangeText={amount => this.setState({ accNumber: amount.replace(" ", "") })}
+              />
+            </View>
+          </View>
+          <View style={{ flexDirection: 'row', width: '70%', marginBottom: 10 }}>
+            <View style={{ paddingTop: width / 28 }}>
+              <Title>Enter Amount: </Title>
+            </View>
+            <View style={{ width: '70%'}}>
+              <TextInput
+                placeholder={'0000000'}
+                // onChangeText={...}
+                autoFocus
+                keyboardType='numeric'
+                onChangeText={amount => this.setState({ amount: amount.replace(" ", "") })}
+              />
             </View>
           </View>
           <View style={{ marginTop: 40 }}>
             <ButtonTextComponent
-              // onPress={this.transferToEmail}
-              buttonText='LOAD'
-              iconName='payment'
+              onPress={this.withdraw}
+              buttonText='TRANSFER'
+              iconName='send'
               iconType='material'
               backgroundColor='#333'
             />
